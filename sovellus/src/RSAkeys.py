d68b6daa74044e4377843e85921dcd829e8b77cc
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2021-11-12 18:17:26 +0200

    alkuluvut löydetty

--- sovellus/src/RSAkeys.py
+++ sovellus/src/RSAkeys.py
@@ -65,8 +65,22 @@ class RSAkeys:
             True, jos luku on hyvin suurella todennäköisyydellä alkuluku.
         """
 
-        i = 0
-        while i < k:
-            a = random.randint(2, n-2)
-            
+        r, d = 0, n-1
+        while d % 2 == 0:
+            r += 1
+            d = d // 2
+
+        for _ in range(k):
+            a = random.randint(2, n-1)
+            x = pow(a, d, n)
+            if x == 1 or x == n-1:
+                continue
+            for _ in range(r-1):
+                x = pow(x, 2, n)
+                if x == n-1:
+                    break
+            else:
+                return False
+        return True
+
 
