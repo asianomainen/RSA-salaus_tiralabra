fefa68929ee336a3bfefeaf5b65232d6a97641ea
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2021-11-15 13:13:19 +0200

    update

--- sovellus/src/RSAkeys.py
+++ sovellus/src/RSAkeys.py
@@ -1,4 +1,5 @@
 import random
+from timeit import default_timer as timer
 
 
 class rsa_keys:
@@ -8,32 +9,49 @@ class rsa_keys:
     Attributes:
         public_key
         private_key
+        lenght: Avainten luomiseen käytettyjen lukujen pituus biteissä.
+                Tässä tapauksessa 1024 bittiä, mikä vastaa 2048 bittiä pitkää avainta.
     """
 
     def __init__(self):
         """Luokan konstruktori.
         """
+
         self.public_key = None
         self.private_key = None
+        self.length = 1024
+
+    def generate_number(self, n):
+        """Luo n-bittiä pitkän luvun.
+        
+        Args: 
+            n: Integer, luvun pituus biteissä
+        
+        Returns: 
+            Integer.
+        """
+
+        return random.getrandbits(n)
 
     def generate_keys(self):
-        """Luo RSA-avainparin
+        """Luo RSA-avainparin.
 
         Returns:
-            RSA-avainparin
+            RSA-avainparin.
         """
 
         p, q = self.generate_prime_numbers()
+        print("done")
 
     def generate_prime_numbers(self):
-        """Luo alkuluvut p ja q, niin että p*q >= 2048 bittiä ja p != q.
+        """Luo alkuluvut p ja q, niin että p != q.
 
         Returns:
             Alkulukuparin.
         """
 
         while True:
-            p, q = random.getrandbits(1030), random.getrandbits(1030)
+            p, q = self.generate_number(self.length), self.generate_number(self.length)
             if p == q or p % 2 == 0 or q % 2 == 0:
                 continue
             if self.is_prime(p) and self.is_prime(q):
@@ -44,13 +62,14 @@ class rsa_keys:
         """Tarkistaako onko luku alkuluku.
 
         Args:
-            n: Integer, jota halutaan testata
+            n: Integer, jota halutaan testata.
 
         Returns: 
             True, jos luku on alkuluku.
         """
 
-        if self.miller_rabin(n, 40):
+
+        if self.miller_rabin(n, 20):
             return True
         return False
 
@@ -83,3 +102,10 @@ class rsa_keys:
             else:
                 return False
         return True
+
+if __name__ == '__main__':
+    start = timer()
+    keys = rsa_keys()
+    keys.generate_keys()
+    end = timer()
+    print(end - start)
---
+++ sovellus/src/smallprimes.py
@@ -0,0 +1,40 @@
+class small_primes:
+    
+    """Luokka pienten alkulukujen luomiseen
+
+    Attributes: 
+        up_till: Integer, mihin asti alkulukuja generoidaan.
+        list_primes: Lista alkuluvuista. 
+    """
+
+    def __init__(self):
+        """Luokan konstruktori.
+        """
+
+        self.up_till = 256
+        self.list_primes = self.generate_list(self.up_till)
+    
+    def generate_list(self, n):
+        """Luo listan alkuluvuista lukuun n asti.
+
+        Args: 
+            n: Integer, mihin asti alkulukuja luodaan.
+        
+        Returns: 
+            Listan alkuluvuista.
+        """
+        
+        primes = [True for i in range(n+1)]
+        for i in range(2, n+1):
+            if primes[i]:
+                for j in range(i*i, n+1, i):
+                    primes[j] = False
+        prime_list = []
+        for i in range(2, n+1):
+            if primes[i]:
+                prime_list.append(i)
+        return prime_list
+                
+
+
+
